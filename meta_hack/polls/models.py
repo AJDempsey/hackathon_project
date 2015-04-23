a33a27068094c9f7e4a8113ec8774ffffae7e323
Anthony Dempsey <anthonyjdempsey@gmail.com>
2015-04-23 23:14:54 +0100

    Started testing
    Added initial test for a bug that exists returing true for dates in the
    future.
    
    Tutorial can be found at:
    https://docs.djangoproject.com/en/1.7/intro/tutorial05/

--- meta_hack/polls/models.py
+++ meta_hack/polls/models.py
@@ -31,7 +31,8 @@ class Question(models.Model):
 
             Return: Boolean
         """
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
